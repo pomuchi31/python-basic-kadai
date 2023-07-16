08dbc09966080a702cc45ef06474b1254fda751b
pomuchi31 <131507761+pomuchi31@users.noreply.github.com>
2023-07-17 07:29:12 +0900

    Add files via upload

--- kadai_015.py
+++ kadai_015.py
@@ -2,12 +2,20 @@ class Human:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
     def set_name(self, name):
         self.name = name
+    
     def show_name(self):
         print(self.name)
-
-user = Human("太郎", 33)  
     
+    def printinfo(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+user = Human("太郎", 33)
+
 print(user.name)
 print(user.age)
+
+user.printinfo()
